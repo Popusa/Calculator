67012bab41ab5873b082b152a92eeaa40b04290f
popusa <danielmaged.3@gmail.com>
2021-11-23 03:31:40 +0200

    changed basic margins and added js boilerplate

---
+++ js/Calc.js
@@ -0,0 +1,21 @@
+const CalcLogic = {
+    OperandOne: 0,
+    OperandTwo: 0,
+    temp: 0,
+    Operator: '',
+    Operate:function(){
+
+    },
+    AddOperands:function(){
+        
+    },
+    SubtractOperands:function(){
+        
+    },
+    MultiplyOperands:function(){
+        
+    },
+    DivideOperands:function(){
+        
+    },
+};
