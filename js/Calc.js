8058555681bce91d1a6ef0637a24d8107aef9fb7
popusa <danielmaged.3@gmail.com>
2021-11-26 00:47:59 +0200

    fixed grid buttons width and implemented functions for buttons

--- js/Calc.js
+++ js/Calc.js
@@ -1,21 +1,38 @@
 const CalcLogic = {
     OperandOne: 0,
     OperandTwo: 0,
-    temp: 0,
+    current:"",
+    total: 0,
     Operator: '',
     Operate:function(){
 
     },
     AddOperands:function(){
-        
+        return this.OperandOne + this.OperandTwo;
     },
     SubtractOperands:function(){
-        
+        return this.OperandONe - this.OperandTwo;
     },
     MultiplyOperands:function(){
-        
+        return this.OperandOne * this.OperandTwo;
     },
     DivideOperands:function(){
-        
+        if (this.OperandTwo == 0)
+            return "I ain't doing that...";
+        else
+            return this.OperandOne / this.OperandTwo;
     },
+    DeleteCurrent:function(){
+        this.current[this.current.length - 1] = "";
+    }, 
+    GetPercent:function(){
+        this.current / 100;
+    },
+    SetNegative:function(){
+        current = -this.current;
+    },
+    ClearAll:function(){
+        this.OperandOne = 0;
+        this.OperandTwo = 0;
+    }
 };
