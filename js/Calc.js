799b5d5077b4353637ec8e45c0a9f6d0d6a8a284
popusa <danielmaged.3@gmail.com>
2021-11-30 18:20:29 +0200

    fixed error in selected elements and added operator elements

--- js/Calc.js
+++ js/Calc.js
@@ -1,19 +1,23 @@
 const displayed = document.querySelector(".display");
-const percentified = document.querySelector(".percent");
-const negativified = document.querySelector(".negative");
-const clearall = doucment.quertySelector("clear");
-const one = document.quertySelector(".one");
-const two = document.quertySelector(".two");
-const three = document.quertySelector(".three");
-const four = document.quertySelector(".four");
-const five = document.quertySelector(".five");
-const six = document.quertySelector(".six");
-const seven = document.quertySelector(".seven");
-const eight = document.quertySelector(".eight");
-const nine = document.quertySelector(".nine");
-const zero = document.quertySelector(".zero");
-const equal = document.querySelector(".equal");
-const removeone = document.querySelector(".delete");
+const percentified = document.querySelector("#percent");
+const negativified = document.querySelector("#negative");
+const clearall = document.querySelector("#clear");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+const zero = document.querySelector("#zero");
+const equal = document.querySelector("#equal");
+const removeone = document.querySelector("#delete");
+const add = document.querySelector("#add");
+const subtract = document.querySelector("#subtract");
+const divide = document.querySelector("#multiply");
+const multiply = document.querySelector("#divide");
 //EVENT LISTENER LOGIC
 percentified.addEventListener('click',function(){
     return current / 100;
@@ -22,7 +26,7 @@ negativified.addEventListener('click',function(){
     return -current;
 });
 clearall.addEventListener('click',function(){
-    displayed.innerText = "0";
+//    displayed.innerText = "0";
     current = "";
     OperandOne = 0;
     OperandTwo = 0;
