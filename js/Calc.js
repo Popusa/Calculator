e1bc0d8f3674397cef72a42e993194f67b3066ac
popusa <danielmaged.3@gmail.com>
2021-11-30 17:47:35 +0200

    added event listeners for misc functions

--- js/Calc.js
+++ js/Calc.js
@@ -1,4 +1,35 @@
 const displayed = document.querySelector(".display");
+const percentified = document.querySelector(".percent");
+const negativified = document.querySelector(".negative");
+const clearall = doucment.quertySelector("clear");
+const one = document.quertySelector(".one");
+const two = document.quertySelector(".two");
+const three = document.quertySelector(".three");
+const four = document.quertySelector(".four");
+const five = document.quertySelector(".five");
+const six = document.quertySelector(".six");
+const seven = document.quertySelector(".seven");
+const eight = document.quertySelector(".eight");
+const nine = document.quertySelector(".nine");
+const zero = document.quertySelector(".zero");
+const equal = document.querySelector(".equal");
+const removeone = document.querySelector(".delete");
+//EVENT LISTENER LOGIC
+percentified.addEventListener('click',function(){
+    return current / 100;
+});
+negativified.addEventListener('click',function(){
+    return -current;
+});
+clearall.addEventListener('click',function(){
+    displayed.innerText = "0";
+    current = "";
+    OperandOne = 0;
+    OperandTwo = 0;
+    total = 0;
+    Operator = '';
+    return true;
+});
 const CalcLogic = {
     OperandOne: 0,
     OperandTwo: 0,
