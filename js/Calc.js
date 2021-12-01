3524045caaeceb1166369729b6df6e286433a465
popusa <danielmaged.3@gmail.com>
2021-12-01 23:17:46 +0200

    added more eventlisteners and fleshed out some parts of the CalcLogic object

--- js/Calc.js
+++ js/Calc.js
@@ -18,30 +18,17 @@ const add = document.querySelector("#add");
 const subtract = document.querySelector("#subtract");
 const divide = document.querySelector("#multiply");
 const multiply = document.querySelector("#divide");
-//EVENT LISTENER LOGIC
-percentified.addEventListener('click',function(){
-    return current / 100;
-});
-negativified.addEventListener('click',function(){
-    return current * -1;
-});
-clearall.addEventListener('click',function(){
-    displayed.innerText = "0";
-    current = "";
-    OperandOne = 0;
-    OperandTwo = 0;
-    total = 0;
-    Operator = '';
-    return true;
-});
 const CalcLogic = {
     OperandOne: 0,
     OperandTwo: 0,
-    current:"",
+    current:"0",
     total: 0,
     Operator: '',
     Operate:function(){
 
+    },
+    UpdateDisplay:function(){
+        displayed.innerText = this.current;
     },
     AddOperands:function(){
         return this.OperandOne + this.OperandTwo;
@@ -60,16 +47,67 @@ const CalcLogic = {
     },
     DeleteCurrent:function(){
         this.current[this.current.length - 1] = "";
+        CalcLogic.UpdateDisplay();
     }, 
     GetPercent:function(){
         this.current / 100;
+        CalcLogic.UpdateDisplay();
     },
     SetNegative:function(){
         current = -this.current;
+        CalcLogic.UpdateDisplay();
     },
-    ClearAll:function(){
+    ClearEverything:function(){
+        this.current = "";
         this.OperandOne = 0;
         this.OperandTwo = 0;
-    }
+        this.total = 0;
+        this.Operator = '';
+        CalcLogic.UpdateDisplay();
+    },
 };
+//EVENT LISTENER LOGIC
+percentified.addEventListener('click',CalcLogic.GetPercent());
+negativified.addEventListener('click',CalcLogic.SetNegative());
+clearall.addEventListener('click',CalcLogic.ClearEverything());
+zero.addEventListener('click',function(){
+    CalcLogic.current += "0";
+    CalcLogic.UpdateDisplay();
+});
+one.addEventListener('click',function(){
+    CalcLogic.current += "1";    
+    CalcLogic.UpdateDisplay();
+});
+two.addEventListener('click',function(){
+    CalcLogic.current += "2";    
+    CalcLogic.UpdateDisplay();
+});
+three.addEventListener('click',function(){
+    CalcLogic.current += "3";    
+    CalcLogic.UpdateDisplay();
+});
+four.addEventListener('click',function(){
+    CalcLogic.current += "4";    
+    CalcLogic.UpdateDisplay();
+});
+five.addEventListener('click',function(){
+    CalcLogic.current += "5";    
+    CalcLogic.UpdateDisplay();
+});
+six.addEventListener('click',function(){
+    CalcLogic.current += "6";    
+    CalcLogic.UpdateDisplay();
+});
+seven.addEventListener('click',function(){
+    CalcLogic.current += "7";    
+    CalcLogic.UpdateDisplay();
+});
+eight.addEventListener('click',function(){
+    CalcLogic.current += "8";    
+    CalcLogic.UpdateDisplay();
+});
+nine.addEventListener('click',function(){
+    CalcLogic.current += "9";    
+    CalcLogic.UpdateDisplay();
+});
 displayed.innerText = "0";
